16d4771023f892da24b14cd3b44b839a8239bafd
Fjolla12 <fjolla.jakupi@student.uni-pr.edu>
2026-05-31 15:28:04 +0200

    HelpController

---
+++ src/controller/HelpController.java
@@ -0,0 +1,99 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+
+import util.LanguageManager;
+
+public class HelpController {
+
+    @FXML
+    private TextArea addHelpText;
+
+    @FXML
+    private TextArea searchHelpText;
+
+    @FXML
+    private TextArea dashboardHelpText;
+
+    @FXML
+    private TextArea shortcutHelpText;
+
+    @FXML
+    private Tab tabAdd;
+
+    @FXML
+    private Tab tabSearch;
+
+    @FXML
+    private Tab tabDashboard;
+
+    @FXML
+    private Tab tabShortcuts;
+
+    public void initialize(){
+        loadTexts();
+
+        src.util.LanguageManager
+                .currentLocaleProperty()
+                .addListener(
+                    (
+                        observable,
+                        oldLocale,
+                        newLocale
+                    ) -> loadTexts()
+                );
+    }
+
+    private void loadTexts(){
+        tabAdd.setText(
+            LanguageManager.getText(
+                "help.tab.search"
+            )
+        );
+
+        tabSearch.setText(
+            LanguageManager.getText(
+                "help.tab.search"
+            )
+        );
+
+        tabDashboard.setText(
+            LanguageManager.getText(
+                "help.tab.dashboard"
+            )
+        );
+        
+        tabShortcuts.setText(
+            LanguageManager.getText(
+                "help.tab.shortcuts"
+            )
+        );
+
+        addHelpText.setText(
+            LanguageManager.getText(
+                "help.add"
+            )
+        );
+
+        searchHelpText.setText(
+            LanguageManager.getText(
+                "help.search"
+            )
+        );
+
+        dashboardHelpText.setText(
+            LanguageManager.getText(
+                "help.dashboard"
+            )
+        );
+
+        shortcutHelpText.setText(
+            LanguageManager.getText(
+                "help.shortcuts"
+            )
+        );
+
+    }
+}
